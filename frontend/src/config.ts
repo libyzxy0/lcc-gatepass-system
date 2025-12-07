6f3498f0e09c5c9b4294bbf7c5e4ed920deb669c
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2025-12-08 00:02:46 +0800

    fix

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,3 @@
-const localServer = "http://localhost:3000/api/v1";
-export const API_BASE_URL = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? localServer :"https://api.lccgatepass.xyz/api/v1";
+const use_local_server = false;
+
+export const API_BASE_URL = use_local_server ? "http://localhost:3000/api/v1" :"https://api.lccgatepass.xyz/api/v1";
