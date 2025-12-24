e025e148758ad99f3a535989dc9758c7a47d3fdc
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2025-12-24 10:31:11 +0800

    telegram notif

--- server/src/controllers/esp.controller.ts
+++ server/src/controllers/esp.controller.ts
@@ -3,7 +3,7 @@ import db from '@/db/drizzle'
 import { student } from '@/db/schema'
 import { eq } from 'drizzle-orm'
 import axios from 'axios'
-
+import { tg_api } from '@/sms-api/tgapi'
 import EspService from '@/services/esp.service'
 
 class ESPController {
@@ -11,14 +11,14 @@ class ESPController {
     try {
       const event = req.body;
       const payload = JSON.parse(event.payload);
-      
-      const localDate = new Date( 
+
+      const localDate = new Date(
         payload.time).toLocaleDateString('en-US', {
-        weekday: 'long',
-        year: 'numeric',
-        day: 'numeric',
-        month: 'long'
-      });
+          weekday: 'long',
+          year: 'numeric',
+          day: 'numeric',
+          month: 'long'
+        });
 
       const localTime = new Date(payload.time).toLocaleTimeString('en-US', {
         hour: '2-digit',
@@ -29,7 +29,7 @@ class ESPController {
 
       if (payload.action == 'SCAN_QR') {
         const data = await EspService.verifyQR(payload.data);
-        
+
         console.log({
           time_in: `${localDate} ${localTime}`,
           detail: {
@@ -41,10 +41,11 @@ class ESPController {
             verified: data.visitor.verified
           }
         });
+        await tg_api(`${encodeURIComponent(`${data.gatepass.purpose} by ${data.visitor.firstname}`)}`);
         res.status(200).json(data);
       }
 
-      
+
     } catch (error) {
       console.error(error.message, error.status || 500);
       res.status(500).json({
