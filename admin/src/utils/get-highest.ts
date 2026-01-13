37433990f04f3f7f2357c98f4e1e8f56348bd3dd
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2026-01-13 10:20:24 +0800

    fix

--- admin/src/utils/get-highest.ts
+++ admin/src/utils/get-highest.ts
@@ -1,19 +1,27 @@
 import { type OverviewCountsType } from '@/api/helpers/overview';
 
-export const getPeopleTypeWithHighestValue = <T extends OverviewCountsType>(data: T): (keyof T)[] => {
-  
-  const obj = {
+type ReturnType = {
+  students: number;
+  visitors: number;
+  staffs: number;
+  other: number;
+}
+
+export const getPeopleTypeWithHighestValue = (
+  data: OverviewCountsType
+): (keyof ReturnType)[] => {
+  const obj: ReturnType = {
     students: data.students_today,
     visitors: data.visitors_today,
     staffs: data.staffs_today,
     other: data.other_people,
-  }
-  
-  const values = Object.values(obj) as number[];
-  
+  };
+
+  const values = Object.values(obj);
   if (values.length === 0) return [];
 
   const maxVal = Math.max(...values);
-  
-  return (Object.keys(obj) as (keyof T)[]).filter(key => obj[key] === maxVal);
+
+  return (Object.keys(obj) as (keyof ReturnType)[])
+    .filter(key => obj[key] === maxVal);
 };
