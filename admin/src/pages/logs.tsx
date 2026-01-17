23670db3147f56794e67a276b987f142c5f972c7
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2026-01-17 17:05:23 +0800

    feat: added badges for logs entry type and visit type

--- admin/src/pages/logs.tsx
+++ admin/src/pages/logs.tsx
@@ -96,16 +96,16 @@ export default function Logs() {
       accessorKey: "entry_type",
       header: "Entry Type",
       cell: (info) => {
-        const value = info.getValue<string | null>() ?? null;
-        return entryTypeBadges[value]
+        const value = info.getValue<'rfid' | 'qr'>();
+        return value ? entryTypeBadges[value] : 'N/A'
       }
     },
     {
       accessorKey: "type",
       header: "Type",
       cell: (info) => {
-        const value = info.getValue<string | null>() ?? null;
-        return typeBadges[value];
+        const value = info.getValue<'student' | 'visitor' | 'staff' | 'guardian'>();
+        return value ? typeBadges[value] : 'N/A'
       }
     },
     {
